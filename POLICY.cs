d084b4a3ec8bce97cfecb739c7cef192bd45d958
Puja Sharma <43954114+puja0902@users.noreply.github.com>
2020-08-12 13:44:51 +0530

    Add files via upload

---
+++ POLICY.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace INSURANCEMGMT
+{
+    public class policyholder
+    {
+
+    }
+    public class benefits
+    {
+    }
+}
---
+++ insurance.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Principal;
+using System.Text;
+
+namespace NsInsurance
+{
+
+    public class Car
+    {
+        public string CarName { get; set; }
+        public int CarId { get; set; }
+        public string CarOwner { get; set; }
+        public string CarCompany { get; set; }
+
+
+    }
+
+    public class ClaimInsurance
+
+    {
+        public int InsuranceAmount { get; set; }
+        public int InsuranceID { get; set; }
+        public int InsuranceExpiry { get; set; }
+    }
+
+    public class Booking
+    {
+        public ClaimInsurance Claimed { get; set; }
+        public int BookingID { get; set; }
+
+    }
+    public class Customers
+    {
+        public string CustomerName;
+        public string CustomerAddress;
+        public int CustomerMobileNum;
+
+        public void makepayment()
+        {
+        }
+    }
+
+    public class Payment : Customers
+
+    {
+        public int paymentID;
+
+        public void GenerateBill()
+        {
+
+        }
+
+    }
+}
