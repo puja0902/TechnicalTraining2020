4c3576682dfc84338a4b340d14c71bd48fd201f0
puja <getpuja11@gmail.com>
2020-08-12 21:02:50 +0530

    first commit

---
+++ PerformanceCheck/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace PerformanceCheck
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stopwatch st = new Stopwatch();
+            st.Start();
+            CheckPerformance();
+            st.Stop();
+            Console.WriteLine(st.ElapsedMilliseconds);
+            st.Restart();
+            st.Start();
+            CheckPerformance();
+            st.Stop();
+            Console.WriteLine(st.ElapsedMilliseconds);
+            st.Restart();
+            st.Start();
+            CheckPerformance();
+            st.Stop();
+            Console.WriteLine(st.ElapsedMilliseconds);
+            st.Restart();
+
+            Console.WriteLine(st.ElapsedMilliseconds);
+            Console.Read();
+            Console.WriteLine("This is Main");
+        }
+
+
+        static void
+        CheckPerformance()
+        {
+        for (int i=0; i<1000000; i++)
+	        {
+                int i1 = 10;
+                object y1 = i;
+             }
+
+    }
+}
+
+
+
+    }
+    
+
