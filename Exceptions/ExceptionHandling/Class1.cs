c077b50005b19b2820cf93dee1e9e0c3930ef67f
Puja Sharma <43954114+puja0902@users.noreply.github.com>
2020-09-02 21:02:35 +0530

    Add files via upload

---
+++ Exceptions/ExceptionHandling/Class1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ExceptionHandling
+{
+    //EXAMPLE OF HANDLED EXCEPTION 
+    class HandledException
+    {
+        public static void Main()
+        {
+            int x = 0;
+            int intTemp = 0;
+            try
+            {
+                intTemp = 100 / x;
+                Console.WriteLine("Program terminated before this statement");
+            }
+            catch (DivideByZeroException de)
+            {
+                Console.WriteLine("Division by Zero occurs");
+            }
+            finally
+            {
+
+                Console.WriteLine("Result is {0}", intTemp);
+            }
+        }
+
+   
+    }
+}
---
+++ Exceptions/UIconsole/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace UIconsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
