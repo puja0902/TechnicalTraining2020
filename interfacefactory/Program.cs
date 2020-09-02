052f05dc28e5a832b5040efd2a5cfc61a9d2190a
Puja Sharma <43954114+puja0902@users.noreply.github.com>
2020-09-02 20:59:04 +0530

    Add files via upload

---
+++ interfacefactory/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+
+namespace interfacefactory
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ITax t = Factory.Create(1);
+            t = Factory.Create(0);
+
+
+
+            t.Calculate();
+
+        }
+    }
+}
